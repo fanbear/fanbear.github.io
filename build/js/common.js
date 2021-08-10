d93996a5cdaf4e17eafacda77f6741f0ba9c0900
Yura <fanbearytb@gmail.com>
2021-08-10 12:29:17 +0300

    adaptive + menu

--- build/js/common.js
+++ build/js/common.js
@@ -1,9 +1,12 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', function() {
+
     //Верхная часть шапки
     const address = document.querySelector('.top__line-text__hover');
     const addressBtn = document.querySelector('.top__line-button__open');
     
-    // шапка сайта
+    // Шапка сайта
     const searchList = document.querySelector('.header-search__category .dropdown-menu');
     const searchBtn = document.querySelector('.header-search__open-category');
 
@@ -11,8 +14,65 @@ document.addEventListener('DOMContentLoaded', function() {
     const tabsHeader = document.querySelectorAll('.product-tabs__header-title');
     const tabsContent = document.querySelectorAll('.product-tabs__content-wrapper');
 
+    //Фильтр открытие и скрытие
+    const filterBtnOpen = document.querySelector('.category-sort__filter');
+    const filterBtnClose = document.querySelector('.filter-module__header-close');
+    const filter = document.querySelector('.modules');
 
+    //Сортировка в категориях
+    const sortBtn = document.querySelector('.category-sort__arrow');
+    const sortList = document.querySelector('.category-sort__list');
+    
 
+    //Меню сайта
+    const nav = document.querySelector('.menu-nav');
+    const navBtnPC = nav.querySelector('.menu-nav__title i');
+    const navMainList = nav.querySelector('.menu-nav__list');
+
+    //открытие основного меню по клику
+    if (navBtnPC) {
+        navBtnPC.addEventListener('click', function() {
+            navMainList.classList.toggle('menu-nav__list-active');
+        })
+    }
+
+    //Открытие вложеных меню по клику
+
+    //Закрыть все активные подменю и убрать фон у елемента li в основном меню
+    function closeAllSubmenu() {
+        const navList = document.querySelectorAll('.menu-nav__list li');
+
+        navList.forEach(function(item) {
+            if (item.classList.contains('submenu-open')) {
+                item.classList.remove('submenu-open');
+                item.querySelector('ul').classList.remove('submenu-active');
+            }
+        })
+    }
+
+    //открыть подменю
+    //добавить класс елементу li (li->backgorund)
+    function openAllSubmenu(el, selector) {
+        console.log(el);
+        el.classList.toggle('submenu-open');
+        if (el.querySelector('ul')) {
+            el.querySelector('ul').classList.toggle(selector)
+        }
+        
+        
+    }
+
+    if (navMainList) {
+        navMainList.addEventListener('click', function(e) {
+
+            const submenu = e.target.closest('li');
+
+            if (submenu) {
+                // closeAllSubmenu();
+                openAllSubmenu(submenu, 'submenu-active');
+            }
+        })
+    }
 
     // elem - элемент который нужно скрыть/отобразить
     // btn - кнопка (img) которой нужно задать действие при открытии
@@ -28,7 +88,59 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    //Модуль табов
+    //Функция изменения позиции меню и фильтра
+    //selector = doc.qS('selector')
+    //active = 'active-menu / active-filter'
+        //Открыть
+    function openHideElement(selector) {
+        return (active) => selector.classList.add(active);
+    }
+        //Закрыть
+    function closeHideElement(selector) {
+        return (active) => selector.classList.remove(active);
+    }
+    
+    
+    //************************* */ 
+    //**** */   События  //**** */
+    //************************* */
+
+    //Открытие адерса в шапке
+    if (addressBtn) {
+        addressBtn.addEventListener('click', function() {
+            toggleSelector(address, addressBtn)
+        })
+    }
+    
+    //Открытие категорий в поиске
+    if (searchBtn) {
+        searchBtn.addEventListener('click', function() {
+            toggleSelector(searchList, searchBtn)
+        })
+    }
+
+    //Открытие фильтра для моб и пк версии
+    if (filterBtnOpen) {
+        filterBtnOpen.addEventListener('click', function(){
+            openHideElement(filter)('open-filter');
+        })
+    }
+    if (filterBtnClose) {
+        filterBtnClose.addEventListener('click', function(){
+            closeHideElement(filter)('open-filter')
+        })
+    }
+
+    //Сортировка в категориях
+    if (window.innerWidth < 478 && sortBtn) {
+        sortBtn.addEventListener('click', function() {
+            sortList.classList.toggle('category-sort__list-active');
+            this.style.marginLeft = 'auto'
+        })
+        
+    }
+
+     //Модуль табов  на главной
     if (tabsHeader) {
 
         // Удаление класса из псевдомасива
@@ -54,16 +166,4 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
     
-
-
-
-    //Открытие адерса в шапке
-    addressBtn.addEventListener('click', function() {
-        toggleSelector(address, addressBtn)
-    })
-
-    //Открытие категорий в поиске
-    searchBtn.addEventListener('click', function() {
-        toggleSelector(searchList, searchBtn)
-    })
 })
